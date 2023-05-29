0b655a579ae5d61d5d961cb0d585d11ecb9b5864
MarcusDantas <marvinsd@gmail.com>
2023-05-29 14:29:12 -0300

    feat: add dashboard to manage orders

--- dashboard.php
+++ dashboard.php
@@ -1,29 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./src/css/reset.css">
-    <link rel="stylesheet" href="./src/css/template.css">
-    <link rel="stylesheet" href="./src/css/index.css">
-    <link rel="stylesheet" href="./src/css/dashboard.css">
-    <title>Marvin's Pizzeria OMS</title>
-</head>
-<body>
-    <header>
-        <a href="" class="logo">
-            <img src="./src/img/marvins-logo.png" alt="Marvin's Pizzeria">
-        </a>
-        <nav>
-            <a href="">Register Order</a>
-            <div class="menu-space"></div>
-            <a href="">Order Management</a>
-        </nav>
-    </header>
+<?php 
+    include_once("./src/template/header.php");
+    include_once("./src/process/orders.php");
+?>
     <main>
         <div class="orders-container">
             <h2>Orders</h2>
@@ -43,25 +21,40 @@
                         </tr>
                     </thead>
                     <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                        <?php foreach($pizzas as $pizza): ?>
+                            <tr class="status-<?= strtolower($pizza["status"]) ?>">
+                                <td>#<?= $pizza["id"] ?></td>
+                                <td><?= $pizza["size"] ?></td>
+                                <td><?= $pizza["crust"] ?></td>
+                                <td>
+                                    <ul>
+                                        <?php foreach($pizza["toppings"] as $topping): ?>
+                                            <li><?= $topping ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </td>
+                                <td>
+                                    <p><?= $pizza["status"] ?></p>
+                                </td>
                                 <td class="last-columns">   
                                     <div class="action-buttons">
-                                        <form method="POST">
+                                        <form action="./src/process/orders.php" method="POST">
+                                            <input type="hidden" name="type" value="prepare">
+                                            <input type="hidden" name="id" value="<?= $pizza["id"] ?>">
                                             <button type="submit" class="prepare-btn">
                                                 <ion-icon name="pizza"></ion-icon>
                                             </button>
                                         </form>
-                                        <form method="POST">
+                                        <form action="./src/process/orders.php" method="POST">
+                                            <input type="hidden" name="type" value="complete">
+                                            <input type="hidden" name="id" value="<?= $pizza["id"] ?>">
                                             <button type="submit" class="complete-btn">
                                                 <ion-icon name="checkmark-circle"></ion-icon>
                                             </button>
                                         </form>
-                                        <form method="POST">
+                                        <form action="./src/process/orders.php" method="POST">
+                                            <input type="hidden" name="type" value="cancel">
+                                            <input type="hidden" name="id" value="<?= $pizza["id"] ?>">
                                             <button type="submit" class="cancel-btn">
                                                 <ion-icon name="close-circle"></ion-icon>
                                             </button>
@@ -69,15 +62,12 @@
                                     </div>
                                 </td>
                             </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table> 
             </div>
         </div>
     </main>
-    <footer>
-        <p>Marvin's Pizzeria &copy; 2023</p>
-    </footer>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-</body>
-</html>
+    <?php 
+    include_once("./src/template/footer.php")
+?> 
